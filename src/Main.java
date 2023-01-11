1f6024cb625dea77bcd4946dddc9648de0cf68fd
binoy <binoy.18@cse.mrt.ac.lk>
2023-01-11 22:12:47 +0530

    arrival times updated

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         // mean time between riders in seconds
-        final float meanRiderArrivalTime = 0.5f;
+        final float meanRiderArrivalTime = 30;
 
         // mean time between buses in seconds
-        final float meanBusArrivalTime = 20;
+        final float meanBusArrivalTime = 1200;
 
         final int busCapacity = 50;
 
